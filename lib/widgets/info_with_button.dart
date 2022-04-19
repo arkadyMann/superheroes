c2dba7d9b9dcbe57da3c732d5454bdb84e0be29b
arkady <arkadiomanucci@gmail.com>
2022-04-19 21:55:11 +0300

    changed name superheroe_card.dart

--- lib/widgets/info_with_button.dart
+++ lib/widgets/info_with_button.dart
@@ -34,7 +34,7 @@ class InfoWithButton extends StatelessWidget {
           children: [
             Container(
               height: 108,
-              width: 84,
+              width: 108,
               decoration: BoxDecoration(
                 shape: BoxShape.circle,
                 color: SuperheroesColors.blue,
